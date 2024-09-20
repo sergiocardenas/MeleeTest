5bed8b1361aa01f2d0e12a03679b4491dc186187
SergioCardenas <sergio.cardenas@globant.com>
2024-09-19 22:31:32 -0500

    Add navigation component

---
+++ app/src/main/java/com/sc/meleetest/fragment/SearchFragment.kt
@@ -0,0 +1,75 @@
+package com.sc.meleetest.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.fragment.findNavController
+import com.sc.meleetest.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SearchFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val composeView = ComposeView(requireContext())
+        composeView.setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.white)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {
+                        goToDetail()
+                    },
+                    modifier = Modifier
+                        .width(150.dp)
+                        .height(48.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = colorResource(id = R.color.mercado_yellow)
+                    )
+                ) {
+                    Text(
+                        text = "Go to DetailFragment",
+                        color = colorResource(id = R.color.white)
+                    )
+                }
+            }
+        }
+        return composeView
+    }
+
+
+    fun goToDetail(){
+        findNavController().navigate(
+            resId = R.id.action_SearchFragment_to_DetailFragment
+        )
+    }
+
+}
