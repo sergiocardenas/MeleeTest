a30b13707418706f84bdf9a7d83d106f4bd70ad1
SergioCardenas <sergio.cardenas@globant.com>
2024-09-20 20:50:02 -0500

    Add compose screens

--- app/src/main/java/com/sc/meleetest/fragment/DetailFragment.kt
+++ app/src/main/java/com/sc/meleetest/fragment/DetailFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
+import com.sc.meleetest.screen.DetailScreen
 
 /**
  * A simple [Fragment] subclass.
@@ -23,7 +23,7 @@ class DetailFragment : Fragment() {
     ): View {
         val composeView = ComposeView(requireContext())
         composeView.setContent {
-            Text(text = "DetailFragment")
+            DetailScreen()
         }
         return composeView
     }
--- app/src/main/java/com/sc/meleetest/fragment/HomeFragment.kt
+++ app/src/main/java/com/sc/meleetest/fragment/HomeFragment.kt
@@ -5,22 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
 import com.sc.meleetest.R
+import com.sc.meleetest.screen.HomeScreen
 
 /**
  * A simple [Fragment] subclass.
@@ -37,29 +25,8 @@ class HomeFragment : Fragment() {
     ): View {
         val composeView = ComposeView(requireContext())
         composeView.setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(colorResource(id = R.color.white)),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = {
-                        goToSearch()
-                    },
-                    modifier = Modifier
-                        .width(150.dp)
-                        .height(48.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = colorResource(id = R.color.mercado_yellow)
-                    )
-                ) {
-                    Text(
-                        text = "Go to SearchFragment",
-                        color = colorResource(id = R.color.white)
-                    )
-                }
+            HomeScreen() {
+                goToSearch()
             }
         }
         return composeView
