8440ccb066baffc033c68656fb266eeea2e694c6
sergiocardenas <sharser2@gmail.com>
2024-09-20 23:16:01 -0500

    Add navigation component (#2)
    * Add splashScreen
    
    * Add navigation component
    
    * Add compose screens
    
    * Add compose UT
    
    * Add navigation UT
    
    ---------
    
    Co-authored-by: SergioCardenas <sergio.cardenas@globant.com>

---
+++ app/src/main/java/com/sc/meleetest/fragment/DetailFragment.kt
@@ -0,0 +1,31 @@
+package com.sc.meleetest.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import com.sc.meleetest.screen.DetailScreen
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val composeView = ComposeView(requireContext())
+        composeView.setContent {
+            DetailScreen()
+        }
+        return composeView
+    }
+
+}
